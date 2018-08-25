3675971a4485446167943b836ff0f8480a565062
smibbie <35998407+smibbie@users.noreply.github.com>
2018-08-25 11:18:19 -0400

    updated budget table

--- database/schema.sql
+++ database/schema.sql
@@ -16,18 +16,19 @@ CREATE TABLE IF NOT EXISTS Users (
 
 -- BUDGETS table is a direct child of the parent
 CREATE TABLE IF NOT EXISTS Budgets (
-    id INTEGER AUTO_INCREMENT NOT NULL,
-    total_balance DECIMAL(10,2) DEFAULT 0,
-    rent DECIMAL(10,2) DEFAULT 0,
-    utilities DECIMAL(10,2) DEFAULT 0,
-    groceries DECIMAL(10,2) DEFAULT 0,
-    entertainment DECIMAL(10,2) DEFAULT 0,
-    medical DECIMAL(10,2) DEFAULT 0,
-    createdAt DATETIME NOT NULL,
-    updatedAt DATETIME NOT NULL,
-    UserId INTEGER,
-    PRIMARY KEY ('id'),
-    FOREIGN KEY (UserId)
-        REFERENCES Users (id)
-        ON DELETE CASCADE
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  total_balance DECIMAL(10,2) DEFAULT 0,
+  rent DECIMAL(10,2) DEFAULT 0,
+  utilities DECIMAL(10,2) DEFAULT 0,
+  groceries DECIMAL(10,2) DEFAULT 0,
+  entertainment DECIMAL(10,2) DEFAULT 0,
+  medical DECIMAL(10,2) DEFAULT 0,
+  createdAt DATETIME NOT NULL,
+  updatedAt DATETIME NOT NULL,
+  UserId INTEGER,
+  PRIMARY KEY ('id'),
+  FOREIGN KEY (UserId)
+    REFERENCES Users (id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
